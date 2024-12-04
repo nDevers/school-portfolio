60107ff006532e46fac59cb165f919ca2f435b3c
AႦιԃ Hαʂαɳ <meghpiash@gmail.com>
2024-12-05 00:24:59 +0600

    update

---
+++ app/(home)/(files)/notice/page.js
@@ -0,0 +1,10 @@
+import PageTitle from '@/components/admin/common/PageTitle'
+import React from 'react'
+
+export default function NoticePage() {
+    return (
+        <div className="w-full h-full max-w-7xl mx-auto sp my-6 space-y-6">
+            <PageTitle title="নোটিশ" />
+        </div>
+    )
+}
