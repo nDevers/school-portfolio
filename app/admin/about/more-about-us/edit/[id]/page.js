186679cbdcc567b0e365c8c53bd1be0eed933929
AႦιԃ Hαʂαɳ <meghpiash@gmail.com>
2024-12-12 20:28:16 +0600

    feat: edit page for faq and about us

--- app/admin/about/more-about-us/edit/[id]/page.js
+++ app/admin/about/more-about-us/edit/[id]/page.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import NoDataFound from '@/components/admin/common/NoDataFound';
+import PageTitle from '@/components/admin/common/PageTitle'
+import CarouselForm from '@/components/admin/form/CarouselForm';
+import apiConfig from '@/configs/apiConfig'
+import { fetchDataAsServer } from '@/util/axios';
 
-export default function FaqEditPage() {
-  return (
-    <div>FaqEditPage</div>
-  )
+export default async function MoreAboutUsEditPage({ params }) {
+    const { id } = params;
+    const data = await fetchDataAsServer(apiConfig?.GET_MORE_ABOUT_US_BY_ID + id)
+
+    return (
+        <div className='space-y-4'>
+            <PageTitle title='Edit More About Us' />
+            {data ? (
+                <CarouselForm data={data} />
+            ) : (
+                <NoDataFound />
+            )}
+        </div>
+    )
+}
+
+// Return a list of `params` to populate the [id] dynamic segment
+export async function generateStaticParams() {
+    const data = await fetchDataAsServer(apiConfig?.GET_MORE_ABOUT_US)
+    return Array.isArray(data) ? data?.map((item) => ({
+        id: encodeURIComponent(item?.id || item?._id),
+    })) : [];
 }
